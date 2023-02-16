0cbe8a21dbd6a97538b9baac41342185d933a3e7
David Pigeon <90649552+DavidPcegep@users.noreply.github.com>
2023-02-16 14:25:40 -0500

    Create Calculatrice.cs

---
+++ Calculatrice.cs
@@ -0,0 +1,57 @@
+Console.WriteLine("----- Bienvenue sur le fameux logiciel de Calculatrice -----");
+Console.WriteLine("----- Vous pouvez faire des opérations de base -----");
+Console.WriteLine("----- Choisissez une opération -----");
+Console.WriteLine("----- 1. Addition -----");
+Console.WriteLine("----- 2. Soustraction -----");
+Console.WriteLine("----- 3. Multiplication -----");
+Console.WriteLine("----- 4. Division ----- \n");
+
+while (true) {
+    Console.Write("Votre choix : ");
+    int reponse = int.Parse(Console.ReadLine());
+
+    double nombre1 = 0;
+    double nombre2 = 0;
+    double resultat = 0;
+    switch (reponse)
+    {
+        case 1:
+            Console.WriteLine("----- Vous avez choisi l'addition -----");
+            Console.Write("Entrez le premier nombre : ");
+            nombre1 = double.Parse(Console.ReadLine());
+            Console.Write("Entrez le deuxième nombre : ");
+            nombre2 = double.Parse(Console.ReadLine());
+            resultat = nombre1 + nombre2;
+            Console.WriteLine("Le résultat est : " + resultat);
+            break;
+        case 2:
+            Console.WriteLine("----- Vous avez choisi la soustraction -----");
+            Console.Write("Entrez le premier nombre : ");
+            nombre1 = double.Parse(Console.ReadLine());
+            Console.Write("Entrez le deuxième nombre : ");
+            nombre2 = double.Parse(Console.ReadLine());
+            resultat = nombre1 - nombre2;
+            Console.WriteLine("Le résultat est : " + resultat);
+            break;
+        case 3:
+            Console.WriteLine("----- Vous avez choisi la multiplication -----");
+            Console.Write("Entrez le premier nombre : ");
+            nombre1 = double.Parse(Console.ReadLine());
+            Console.Write("Entrez le deuxième nombre : ");
+            nombre2 = double.Parse(Console.ReadLine());
+            resultat = nombre1 * nombre2;
+            Console.WriteLine("Le résultat est : " + resultat);
+            break;
+        case 4:
+            Console.WriteLine("----- Vous avez choisi la division -----");
+            Console.Write("Entrez le premier nombre : ");
+            nombre1 = double.Parse(Console.ReadLine());
+            Console.Write("Entrez le deuxième nombre : ");
+            nombre2 = double.Parse(Console.ReadLine());
+            resultat = nombre1 / nombre2;
+            Console.WriteLine("Le résultat est : " + resultat);
+            break;
+        default:
+            break;
+    }
+}
